a61a863b9d7b8322df8478980288359296ba19bb
Nolan Okey <okeynolan006@gmail.com>
2017-03-28 19:34:48 +0000

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170322210310) do
 
-  create_table "test_models", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
