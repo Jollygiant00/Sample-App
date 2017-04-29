2c7673a744f98fabe001e2ce370228ee2c37fd38
Nolan Okey <okeynolan006@gmail.com>
2017-04-29 21:56:10 +0000

    Corrected host issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'jollygiant.herokuapp.com'
+  host = 'blooming-headland-37969.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
